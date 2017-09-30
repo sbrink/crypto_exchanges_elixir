b428e08e099f8ad1bb4810b0e8e404d0ec00e864
Leo <leonardthiele@googlemail.com>
2017-09-30 20:40:42 +0200

    fix tab size

--- lib/crypto_exchanges/adapters/hitbtc_adapter.ex
+++ lib/crypto_exchanges/adapters/hitbtc_adapter.ex
@@ -1,34 +1,34 @@
 defmodule CryptoExchanges.HitbtcAdapter do
-    @moduledoc """
-    An Adapter for the Hitbtc Exchange
+  @moduledoc """
+  An Adapter for the Hitbtc Exchange
 
-    Homepage: https://hitbtc.com/
-    API Docs: https://hitbtc.com/api
+  Homepage: https://hitbtc.com/
+  API Docs: https://hitbtc.com/api
 
-    Country: -
-    """
-    @behaviour CryptoExchanges.Adapter
+  Country: -
+  """
+  @behaviour CryptoExchanges.Adapter
 
-    alias CryptoExchanges.CryptoCurrency
+  alias CryptoExchanges.CryptoCurrency
 
-    def coinlist do
-      api_get_markets()
-      |> get_in(["symbols"])
-      |> Enum.map(&transform_hitbtc_currency/1)
-      |> Enum.uniq_by(&(&1.symbol))
-    end
+  def coinlist do
+    api_get_markets()
+    |> get_in(["symbols"])
+    |> Enum.map(&transform_hitbtc_currency/1)
+    |> Enum.uniq_by(&(&1.symbol))
+  end
 
-    def transform_hitbtc_currency(hitbtc_currency) do
-      %CryptoCurrency{
-        active: true,
-        symbol: hitbtc_currency["commodity"]
-      }
-    end
+  def transform_hitbtc_currency(hitbtc_currency) do
+    %CryptoCurrency{
+      active: true,
+      symbol: hitbtc_currency["commodity"]
+    }
+  end
 
-    # Private functions
-    @url "https://api.hitbtc.com/api/1/public/symbols"
-    defp api_get_markets do
-      HTTPoison.get!(@url).body
-      |> Poison.decode!
-    end
+  # Private functions
+  @url "https://api.hitbtc.com/api/1/public/symbols"
+  defp api_get_markets do
+    HTTPoison.get!(@url).body
+    |> Poison.decode!
+  end
 end
