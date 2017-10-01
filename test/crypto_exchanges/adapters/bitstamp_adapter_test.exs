1232d6067a6802ea415361d487d4ff2d8351ef35
Sascha Brink <sascha.brink@gmail.com>
2017-10-01 20:36:09 +0200

    Refactor bitstramp test

--- test/crypto_exchanges/adapters/bitstamp_adapter_test.exs
+++ test/crypto_exchanges/adapters/bitstamp_adapter_test.exs
@@ -5,9 +5,7 @@ defmodule CryptoExchanges.BitstampAdapterTest do
 
   describe "#get_currencies" do
     test "get the list" do
-      use_cassette "bitstamp#get_currencies" do
-        assert BitstampAdapter.get_currencies() |> List.first == %CryptoExchanges.CryptoCurrency{active: true, symbol: "BTC"}
-      end
+      assert BitstampAdapter.get_currencies() |> List.first == %CryptoExchanges.CryptoCurrency{active: true, symbol: "BTC"}
     end
   end
 end
