5a81753fbcb9deceb67ccee33c98470505e1feef
Sascha Brink <sascha.brink@gmail.com>
2017-10-01 16:29:36 +0200

    Fix spaces in hitbtc adapter testg

--- test/crypto_exchanges/adapters/hitbtc_adapter_test.exs
+++ test/crypto_exchanges/adapters/hitbtc_adapter_test.exs
@@ -1,13 +1,13 @@
 defmodule CryptoExchanges.HitbtcAdapterTest do
   use CryptoExchanges.AdapterCase
 
-    alias CryptoExchanges.HitbtcAdapter
+  alias CryptoExchanges.HitbtcAdapter
 
-    describe "#coinlist" do
-        test "get the list" do
-            use_cassette "hitbtc#coinlist" do
-                assert HitbtcAdapter.coinlist |> List.first == %CryptoExchanges.CryptoCurrency{active: true, symbol: "BCN"}
-            end
-        end
+  describe "#coinlist" do
+    test "get the list" do
+      use_cassette "hitbtc#coinlist" do
+        assert HitbtcAdapter.coinlist |> List.first == %CryptoExchanges.CryptoCurrency{active: true, symbol: "BCN"}
+      end
     end
+  end
 end
