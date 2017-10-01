843e8b39c50dedf38d318eddb5afbd4080b31c4e
Sascha Brink <sascha.brink@gmail.com>
2017-10-01 20:13:14 +0200

    Move struct to own folder

--- lib/crypto_exchanges/crypto_exchanges.ex
+++ lib/crypto_exchanges/crypto_exchanges.ex
@@ -2,17 +2,4 @@ defmodule CryptoExchanges do
   @moduledoc """
   Documentation for CryptoExchanges.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> CryptoExchanges.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
